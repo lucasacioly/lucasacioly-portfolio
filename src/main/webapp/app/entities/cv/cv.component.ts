cd3a0a480773362482157a59d7dc015a36fa2760
Lucas Acioly <lucasacioly@outlook.com>
2026-04-12 21:15:29 -0300

    feat: download pdf cv

--- src/main/webapp/app/entities/cv/cv.component.ts
+++ src/main/webapp/app/entities/cv/cv.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { TranslateDirective } from 'app/shared/language';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'jhi-cv',
   standalone: true,
-  imports: [TranslateDirective],
+  imports: [TranslateDirective, ButtonModule],
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.scss'
+  styleUrl: './cv.component.scss',
 })
 export class CvComponent {
-
+  downloadPdf(): void {
+    window.print();
+  }
 }
